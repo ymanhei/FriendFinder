9dfe7538a73991904916737f06bdd4b21a85c7dc
Shing Chan <ymanhei@gmail.com>
2019-12-17 13:30:30 +0800

    final draft3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var friends = require("./app/data/friends.js");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
